4e60cc068791ce2f6c1c9c90cd1fdb07ef907e33
László-zsolt Szabadi <42877326+Pliexe@users.noreply.github.com>
2020-12-27 21:06:32 +0100

    ide gas

--- Game/WinMain.cpp
+++ Game/WinMain.cpp
@@ -1,4 +1,21 @@
 #include <Windows.h>
+#include <iostream>
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		std::cout << "ALO BRE TEST" << std::endl;
+		PostQuitMessage(69);
+		break;
+	case WM_KEYDOWN:
+		std::cout << "ALO BRE" << std::endl;
+		if(wParam == 'Q') PostQuitMessage(420);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -11,7 +28,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WndProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -37,7 +54,20 @@ int CALLBACK WinMain(
 
 	ShowWindow(hWnd, nCmdShow);
 
-	while (true);
+	// message pump
+	MSG msg;
+	BOOL gResult;
+	while (gResult = GetMessage(&msg, nullptr, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
-	return 0;
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else {
+		return msg.wParam;
+	}
 }
